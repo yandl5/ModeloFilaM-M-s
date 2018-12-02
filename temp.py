379cd58adff5515ebc3a024655942a28c6ec30af
yanC <yancarlosrocha@gmail.com>
2018-12-02 18:54:55 -0300

    Pendulo migracional

--- temp.py
+++ temp.py
@@ -4,6 +4,7 @@ Spyder Editor
 
 Este é um arquivo de script temporário.
 """
+import numpy as np
 # %% Função fatorial python
 def fatorial(n):
     if n == 0 or n == 1:
@@ -29,8 +30,34 @@ def tempoMedioFila(nGuiches,aHoraGuiche,tClientesHora):
     tempoMedio=(probabilidadeOcupacao(nGuiches,tUtilizacao)/(1-tUtilizacao))*tUtilizacao
     print(tempoMedio)
     return tempoMedio
-# %% dados base
-numeroGuiches = 4
+# %% dados base bancos cidade A
+numeroGuichesBancos = 4
 atendimentoHoraGuiche = 20
 taxaClientesHora = 70
-tempoFila=tempoMedioFila(numeroGuiches,atendimentoHoraGuiche,taxaClientesHora) 
+# %% Cidades
+cidadeA = 803739
+cidadeB = 202456
+cidadeC = 76801
+cidadeD = 23704
+cidadeE = 43191
+#estipular o fenômeno da migração pendular de 10% dos munícipios para A
+#durante o periodo de 8:00 até 16:00, somando 8 horas, logo teremos o centro da
+#metrópole com o total de habitantes nesse dado período de:
+# %% centro da metrópole
+centroMetropole=(cidadeA*0.9)+(cidadeB*0.1)+(cidadeC*0.1)+(cidadeD*0.1)+(cidadeE*0.1)
+centroMetropoleHora = [0]*8
+for i in range(0,8):
+    centroMetropoleHora[i] = np.random.randint(centroMetropole*0.0005,centroMetropole*0.001)
+# %% bancos da metrópole
+numeroGuiches = [4]*5
+# %% Media Hora na cidade 
+MediaHora = [0]*8
+# %%
+for i in range(0,8):
+    for j in range(0,5):
+        MediaHora[i]=MediaHora[i]+tempoMedioFila(numeroGuiches[j],atendimentoHoraGuiche,(centroMetropoleHora[i]/5))
+for i in range(0,8): 
+    MediaHora[i]=MediaHora[i]/5
+    
+    
+    
